ea1b4a3742bb06d542f025ab04d4c5a32e885582
Hadrien Wild <hadrien.wild@gmail.com>
2014-09-09 14:44:47 +0200

    cleaner EndGamePopup code

--- mini-projects/pong/main.py
+++ mini-projects/pong/main.py
@@ -7,7 +7,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 from kivy.uix.popup import Popup
 from kivy.core.audio import SoundLoader
-from kivy.properties import NumericProperty, StringProperty
+from kivy.properties import NumericProperty, StringProperty, ObjectProperty
 from kivy.clock import Clock
 from kivy.vector import Vector
 
@@ -26,7 +26,7 @@ class Ball(Image):
 		"""
 		self.pos = Vector(self.pos) + Vector(self.velocity)
 
-############################# Root Widget Class ##############################
+############################# Main Widget Class ##############################
 
 class PongGame(FloatLayout):
 	def __init__(self, **kwargs):
@@ -89,10 +89,10 @@ class PongGame(FloatLayout):
 		elif self.ball.x < 0:
 			SoundLoader.load('./sounds/goal.wav').play()
 			Clock.unschedule(self.update_game) # stop updating the game
-			self.player_left_score += 1 # update score of the winner
+			self.player_right_score += 1 # update score of the winner
 
-			if self.player_left_score == self.max_score:
-				EndGamePopup().open("player_left", self.racket_left.color)
+			if self.player_right_score == self.max_score:
+				EndGamePopup().open()
 			else:
 				self.serve_ball() # start a new game
 
@@ -100,28 +100,30 @@ class PongGame(FloatLayout):
 		elif self.ball.right > self.width:
 			SoundLoader.load('./sounds/goal.wav').play()
 			Clock.unschedule(self.update_game)
-			self.player_right_score += 1
+			self.player_left_score += 1
 
-			if self.player_right_score == self.max_score:
-				EndGamePopup().open("player_right", self.racket_right.color)
+			if self.player_left_score == self.max_score:
+				EndGamePopup().open()
 			else:
 				self.serve_ball()
 
 ############################### EndGame Popup ################################
 
 class EndGamePopup(Popup):
-	def open(self, winner, winner_color, *args):
+	def on_open(self, *args):
 		"""
 		Opens a popup showing the winning player, and gives the choice to either:
 		play again, go back to the menu, or quit the game.
 		"""
-		super(EndGamePopup, self).open()
-		self.popup_label.color = winner_color
+		# allow to access the main widget (PongGame) instance attributes
+		app = App.get_running_app()
 
-		if winner == "player_left":
+		if app.ponggame.player_left_score > app.ponggame.player_right_score:
+			self.popup_label.color = app.ponggame.racket_left.color
 			self.popup_label.text = "Left Player WON !"
-		elif winner == "player_right":
-			self.popup_label.text = "Right Player WON !"
+		else:
+			self.popup_label.color = app.ponggame.racket_right.color
+			self.popup_label.text = "Right Player WON !"""
 
 ##############################################################################
 
@@ -130,9 +132,9 @@ class KivyApp(App):
 		self.title = "Pong"
 		Builder.load_file('ui.kv')
 
-		pong = PongGame()
-		pong.serve_ball()
-		return pong
+		self.ponggame = PongGame()
+		self.ponggame.serve_ball()
+		return self.ponggame
 
 if __name__ == '__main__':
 	KivyApp().run()
